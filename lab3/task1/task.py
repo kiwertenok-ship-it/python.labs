b8220aa827fd48a9718109e38aca09824e6e7634
kiwert <kiwertenok@gmail.com>
2026-03-29 18:51:55 +0300

    fix: исправлен метод point_in_figure

--- lab3/task1/task.py
+++ lab3/task1/task.py
@@ -5,6 +5,7 @@ class Figure(abc.ABC):
     def __init__(self):
         self.coord = []
 
+    @abc.abstractmethod
     def point_in_figure(self, xp, yp, coord):
         pass
 
@@ -15,12 +16,14 @@ class Figure(abc.ABC):
                 self.coord[i][j] += coord_changes[j]
 
     def is_intersect(self, other):
-        if any(self.point_in_figure(x, y, self.coord) for x, y in other.coord) \
-                and any(not (self.point_in_figure(x, y, self.coord)) for x, y in other.coord):
+        other_points_inside = [self.point_in_figure(x, y, self.coord) for x, y in other.coord]
+        if any(other_points_inside) and not all(other_points_inside):
             return True
-        if any(other.point_in_figure(x, y, other.coord) for x, y in self.coord) \
-                and (any(not (other.point_in_figure(x, y, other.coord)) for x, y in self.coord)):
+
+        self_points_inside = [other.point_in_figure(x, y, other.coord) for x, y in self.coord]
+        if any(self_points_inside) and not all(self_points_inside):
             return True
+
         return False
 
 
@@ -30,11 +33,13 @@ class Quad(Figure):
         super().__init__()
         self.coord = [h1, h2, h3, h4]
 
-    @staticmethod
-    def point_in_figure(xp, yp, coord):
-        if (coord[0][0]) <= xp <= (coord[1][0]) and (coord[0][1] <= yp <= coord[2][1]):
-            return True
-        return False
+    def point_in_figure(self, xp, yp, coord):
+        min_x = min(coord[0][0], coord[1][0], coord[2][0], coord[3][0])
+        max_x = max(coord[0][0], coord[1][0], coord[2][0], coord[3][0])
+        min_y = min(coord[0][1], coord[1][1], coord[2][1], coord[3][1])
+        max_y = max(coord[0][1], coord[1][1], coord[2][1], coord[3][1])
+
+        return min_x <= xp <= max_x and min_y <= yp <= max_y
 
 
 class Pentagon(Figure):
@@ -43,14 +48,17 @@ class Pentagon(Figure):
         super().__init__()
         self.coord = [h1, h2, h3, h4, h5]
 
-    @staticmethod
-    def point_in_figure(xp, yp, coord):
+    def point_in_figure(self, xp, yp, coord):
         ans = False
-        for i in range(len(coord)):
-            if (((coord[i][1] <= yp <= coord[i - 1][1]) or (coord[i - 1][1] <= yp <= coord[i][1])) and
-                    (xp >= (coord[i - 1][0] - coord[i][0]) * (yp - coord[i][1]) / (coord[i - 1][1] - coord[i][1]) + coord[i][1])):
+        n = len(coord)
+        for i in range(n):
+            x1, y1 = coord[i]
+            x2, y2 = coord[(i + 1) % n]
+
+            if ((y1 > yp) != (y2 > yp)) and (xp < (x2 - x1) * (yp - y1) / (y2 - y1) + x1):
                 ans = not ans
         return ans
+
 
 f1 = Quad([0, 0], [1, 0], [1, 1], [0, 1])
 print("Квадрат - ", *f1.coord)
